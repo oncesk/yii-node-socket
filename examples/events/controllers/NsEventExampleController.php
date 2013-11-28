caf91f257f4d3c757a1acad08865b053a3ee4d2b
Once <unnfly@gmail.com>
2013-11-28 12:08:25 +0300

    implement room event example

--- examples/events/controllers/NsEventExampleController.php
+++ examples/events/controllers/NsEventExampleController.php
@@ -23,6 +23,18 @@ class NsEventExampleController extends Controller {
 	}
 
 	public function actionSendRoomEvent() {
+		$event = Yii::app()->nodeSocket->createEventFrame();
+
+		$event->setRoom('example');
+		$event->setEventName('example.room.event');
+
+		$event['type_string'] = 'hello world';
+		$event['type_array'] = array(1, 2, 3);
+		$event['type_object'] = array('one' => 1, 'two' => 2);
+		$event['type_bool'] = true;
+		$event['type_integer'] = 11;
+
+		$event->send();
 		$this->render('sendRoomEvent');
 	}
 
